c75ae152cb79b66b110afe3a1cea983a3249ad43
Tyler Daddio <s.github@tylerdaddio.com>
2021-01-24 19:46:31 -0500

    Optimize Bresenham's line drawing algorithm

--- teensyraster.js
+++ teensyraster.js
@@ -1,17 +1,16 @@
+const BLACK = rgb(0, 0, 0);
+const WHITE = rgb(255, 255, 255);
+const YELLOW = rgb(255, 255, 100);
+const RED = rgb(255, 0, 0);
+const GREEN = rgb(0, 255, 0);
+const BLUE = rgb(0, 0, 255);
+
+let MODEL;
+
 let frame = document.getElementById('frame');
 let context = frame.getContext('2d');
-const WIDTH = frame.width;
-const HEIGHT = frame.height;
-const img = context.createImageData(WIDTH, HEIGHT);
-
-const BLACK = fromRgb(0, 0, 0);
-const WHITE = fromRgb(255, 255, 255);
-const YELLOW = fromRgb(255, 255, 100);
-const RED = fromRgb(255, 0, 0);
-const GREEN = fromRgb(0, 255, 0);
-const BLUE = fromRgb(0, 0, 255);
-
-const framebuffer = makeBuffer(WIDTH*HEIGHT, BLACK);
+const surface = context.createImageData(frame.width, frame.height);
+const framebuffer = makeBuffer(frame.width*frame.height, BLACK);
 
 document.addEventListener('keydown', function(e) {
   if(e.code == 'KeyR') {
@@ -19,14 +18,14 @@ document.addEventListener('keydown', function(e) {
   }
 });
 
-let MODEL;
-fetchObj('https://raw.githubusercontent.com/ssloy/tinyrenderer/f6fecb7ad493264ecd15e230411bfb1cca539a12/obj/african_head.obj')
-  .then((obj) => {
-    console.log(`loaded ${obj.vertices.length} vertices and ${obj.faces.length} faces`)
-    MODEL = obj;
-    draw();
-  });
-
+function init() {
+  fetchText('https://raw.githubusercontent.com/ssloy/tinyrenderer/f6fecb7ad493264ecd15e230411bfb1cca539a12/obj/african_head.obj')
+    .then((text) => {
+      MODEL = parseObj(text);
+      console.log(`loaded ${MODEL.vertices.length} vertices and ${MODEL.faces.length} faces`);
+      draw();
+    });
+}
 
 function draw() {
   let start = new Date().getTime();
@@ -34,47 +33,36 @@ function draw() {
   for(let i = 0; i < MODEL.faces.length; i++) {
     let face = MODEL.faces[i];
     let triangle = {
+      v0: MODEL.vertices[face.v0i],
       v1: MODEL.vertices[face.v1i],
-      v2: MODEL.vertices[face.v2i],
-      v3: MODEL.vertices[face.v3i]
+      v2: MODEL.vertices[face.v2i]
     };
-    bresenhamWireframe(triangle, YELLOW, framebuffer, WIDTH);
+    if(Math.random() > 0.66) {
+      wireframe(triangle, WHITE, framebuffer, frame.width);
+    }else if(Math.random() > 0.5) {
+      wireframe(triangle, GREEN, framebuffer, frame.width);
+    }else{
+      wireframe(triangle, BLUE, framebuffer, frame.width);
+    }
   }
+  let endDraw = new Date().getTime();
   blit(framebuffer);
   let end = new Date().getTime();
-  console.log(end-start);
+  console.log(`Total: ${end-start}, Draw: ${endDraw-start}`);
 }
 
-async function fetchObj(url) {
-  let res = await fetch(url);
-  let text = await res.text();
-  let vertices = [];
-  let faces = [];
-  for(let ln of text.split('\n')) {
-    if(ln.startsWith('v')) {
-      let vtext = ln.split(' ');
-      let x = (+vtext[1] + 1)/2 * HEIGHT;
-      let y = HEIGHT-((+vtext[2] + 1)/2 * HEIGHT);
-      let z = (+vtext[3] + 1);
-      vertices.push({ x, y, z });
-    }
-    if(ln.startsWith('f')) {
-      let words = ln.split(' ');
-      let v1i = +words[1].split('/')[0]-1;
-      let v2i = +words[2].split('/')[0]-1;
-      let v3i = +words[3].split('/')[0]-1;
-      faces.push({ v1i, v2i, v3i });
-    }
-  }
-  return { vertices, faces };
+function wireframe(triangle, color, buffer, width) {
+  line(triangle.v0, triangle.v1, color, buffer, width);
+  line(triangle.v1, triangle.v2, color, buffer, width);
+  line(triangle.v2, triangle.v0, color, buffer, width);
 }
 
-function bresenhamLine(v0, v1, color, buffer, width) {
+function line(v0, v1, color, buffer, width) {
   let isSteep = false;
-  let x0 = Math.floor(v0.x);
-  let y0 = Math.floor(v0.y);
-  let x1 = Math.floor(v1.x);
-  let y1 = Math.floor(v1.y);
+  let x0 = Math.round(v0.x);
+  let y0 = Math.round(v0.y);
+  let x1 = Math.round(v1.x);
+  let y1 = Math.round(v1.y);
   if(Math.abs(x0-x1) < Math.abs(y0-y1)) {
     isSteep = true;
     let swap0 = x0; x0 = y0; y0 = swap0;
@@ -84,75 +72,22 @@ function bresenhamLine(v0, v1, color, buffer, width) {
     let swapX = x0; x0 = x1; x1 = swapX;
     let swapY = y0; y0 = y1; y1 = swapY;
   }
-  let lineWidth = x1-x0;
+  let dx = x1-x0;
+  let dy = y1-y0;
+  let derror = Math.abs(dy) * 2;
+  let error = 0;
+  let y = y0;
+  let yinc = (y1>y0) ? 1 : -1;
   for(let x = x0; x <= x1; x++) {
-    let t = (x-x0) / lineWidth;
-    let y = Math.floor(y0*(1-t) + y1*t);
     if(isSteep) {
       buffer[y+x*width] = color;
     }else{
       buffer[x+y*width] = color;
     }
-  }
-}
-
-function naiveLine(v1, v2, color, buffer, width) {
-  let v1x = v1.x;
-  let v1y = v1.y;
-  let v2x = v2.x;
-  let v2y = v2.y;   
-  for(let t = 0; t < 1; t += 0.1) {
-    let x = Math.floor(v1x + (v2x-v1x) * t);
-    let y = Math.floor(v1y + (v2y-v1y) * t);
-    buffer[x+y*width] = color;
-  }
-}
-
-function bresenhamWireframe(triangle, color, buffer, width) {
-  bresenhamLine(triangle.v1, triangle.v2, color, buffer, width);
-  bresenhamLine(triangle.v2, triangle.v3, color, buffer, width);
-  bresenhamLine(triangle.v3, triangle.v1, color, buffer, width);
-}
-
-/**
- * Draw a triangle using three linear equations. This function assumes the 
- * triangle is acute, that its vertices are not axis-aligned, that the triangle
- * is not degenerate, that anti-aliasing is not required, that the vertices are 
- * provided in a counter-clockwise winding order, and that pixels should be lit
- * only if the center of the pixel falls within the triangle. In other words, 
- * it's not a good function.
- * @param {Object} triangle Triangle to rasterize
- * @param {Number} color color to draw triangle with
- * @param {Number[]} buffer buffer to rasterize into
- * @param {Number} width width of buffer
- * @param {Number} height height of buffer
- */
-function naiveTriangle(triangle, color, buffer, width, height) {
-  let v0 = triangle[0];
-  let v1 = triangle[1];
-  let v2 = triangle[2];
-  let x0 = v0.x;
-  let y0 = v0.y;
-  let x1 = v1.x;
-  let y1 = v1.y;
-  let x2 = v2.x;
-  let y2 = v2.y;
-// y-y1 = m(x-x1)
-// y = mx - mx1 + y1
-// b = -mx1 + y1
-  let m1 = (y0 - y1) / (x0 - x1);
-  let b1 = (-m1 * x0 + y0);
-  let m2 = (y0 - y2) / (x0 - x2);
-  let b2 = (-m2 * x0 + y0);
-  let m3 = (y1 - y2) / (x1 - x2);
-  let b3 = (-m3 * x1 + y1);
-  for(let y = 0; y < height; y++) {
-    for(let x = 0; x < width; x++) {
-      let yy = y+0.5;
-      let xx = x+0.5;
-      if((yy > m1 * xx + b1) && (yy < m2 * xx + b2) && (yy < m3 * xx + b3)) {
-        buffer[x+y*width] = color;
-      }
+    error += derror;
+    if(error > dx) {
+      y += yinc;
+      error -= dx*2;
     }
   }
 }
@@ -167,37 +102,12 @@ function blit(buffer) {
   for(let i = 0; i < buffer.length; i++) {
     let p = i*4;
     let color = buffer[i];
-    img.data[p+0] = (color >> 16) & 0xFF;
-    img.data[p+1] = (color >> 8) & 0xFF;
-    img.data[p+2] = (color >> 0) & 0xFF;
-    img.data[p+3] = 255;
+    surface.data[p+0] = (color >> 16) & 0xFF;
+    surface.data[p+1] = (color >> 8) & 0xFF;
+    surface.data[p+2] = (color >> 0) & 0xFF;
+    surface.data[p+3] = 255;
   }
-  context.putImageData(img, 0, 0);
-}
-
-/**
- * Convert a tuple (r,g,b) into an integer representation of that color that can
- * be stored in a buffer.
- * @param {Number} red 8-bit red value
- * @param {Number} green 8-bit green value
- * @param {Number} blue 8-bit blue value
- * @returns {Number} a 24-bit integer representing the (r,g,b) tuple
- */
-function fromRgb(red, green, blue) {
-  return (red << 16) | (green << 8) | (blue << 0);
-}
-
-/**
- * Convert a 24-bit integer stored in a buffer into an (r,g,b) tuple.
- * @param {Number} color a 24-bit integer representing an (r,g,b) tuple
- * @returns {Object} an (r,g,b) tuple
- */
-function toRgb(color) {
-  return {
-    r: (color >> 16) & 0xFF,
-    g: (color >> 8) & 0xFF,
-    b: (color >> 0) & 0xFF
-  };
+  context.putImageData(surface, 0, 0);
 }
 
 /**
@@ -221,4 +131,44 @@ function makeBuffer(size, initialValue) {
     buffer.push(initialValue);
   }
   return buffer;
+}
+
+/**
+ * Convert a tuple (r,g,b) into an integer representation of that color that can
+ * be stored in a buffer.
+ * @param {Number} red 8-bit red value
+ * @param {Number} green 8-bit green value
+ * @param {Number} blue 8-bit blue value
+ * @returns {Number} a 24-bit integer representing the (r,g,b) tuple
+ */
+function rgb(red, green, blue) {
+  return (red << 16) | (green << 8) | (blue << 0);
+}
+
+function parseObj(text) {
+  let vertices = [];
+  let faces = [];
+  for(let ln of text.split('\n')) {
+    if(ln.startsWith('v')) {
+      let vtext = ln.split(' ');
+      let x = (+vtext[1] + 1) / 2 * frame.height;
+      let y = frame.height - (+vtext[2] + 1) / 2 * frame.height;
+      let z = +vtext[3];
+      vertices.push({ x, y, z });
+    }
+    if(ln.startsWith('f')) {
+      let words = ln.split(' ');
+      let v0i = +words[1].split('/')[0]-1;
+      let v1i = +words[2].split('/')[0]-1;
+      let v2i = +words[3].split('/')[0]-1;
+      faces.push({ v0i, v1i, v2i });
+    }
+  }
+  return { vertices, faces };
+}
+
+async function fetchText(url) {
+  let res = await fetch(url);
+  let text = await res.text();
+  return text;
 }
